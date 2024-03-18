152d9fc430d6cb3012e5e28a737b12059a163896
石津真弥 <ishidushinya@ishidzumayanoMacBook-Pro.local>
2024-03-18 17:48:42 +0900

    chepter4.rb メソッドの作成練習

--- chapter4.rb
+++ chapter4.rb
@@ -1,9 +1,5 @@
-pattern = Regexp.new(ARGV[0])
-filename = ARGV[1]
-
-file = File.open(filename)
-file.each_line do |line|
-    if pattern =~ line
-    end
+def hello
+    puts "Hello, Ruby."
 end
-file.close
+
+hello()
