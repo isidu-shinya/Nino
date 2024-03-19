38c239e81f9d701b40cd24a549b02807fd5bed23
石津真弥 <ishidushinya@ishidzumayanoMacBook-Pro.local>
2024-03-19 20:23:45 +0900

    chapter4別のファイルを取り込む

--- chapter4.rb
+++ chapter4.rb
@@ -1,5 +1,13 @@
-def hello
-    puts "Hello, Ruby."
+def chapter4(pattern, filename)
+ file = File.open(filename)
+ file.each_line do |line|
+    if pattern =~ line
+        print line
+    end
+end
+file.close
 end
 
-hello()
+pattern = Regexp.new(ARGV[0])
+filename = ARGV[1]
+chapter4(pattern, filename)
