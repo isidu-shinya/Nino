8fb0462c41b23488e1f8966bded80e770a3434e2
石津真弥 <ishidushinya@ishidzumayanoMacBook-Pro.local>
2024-03-20 11:08:49 +0900

    chapter4.rb p_books

--- chapter4.rb
+++ chapter4.rb
@@ -1,6 +1,6 @@
 books = [
-    {title: "猫街", author: "萩原？太郎"},
-    {title "猫の事務所", author: "宮沢賢治"},
+    {title: "猫街", author: "萩原何とか太郎"},
+    {title: "猫の事務所", author: "宮沢賢治"},
     {title: "猫語の教科書", author: "ポール、ギャリコ"},
 ]
 p books
