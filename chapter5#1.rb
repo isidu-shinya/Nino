a3014b00047dee03e9f80f1cf08550346153236f
石津真弥 <ishidushinya@ishidzumayanoMacBook-Pro.local>
2024-03-21 12:31:20 +0900

    chapter5多重代入

--- chapter5#1.rb
+++ chapter5#1.rb
@@ -1,4 +1,4 @@
-first, second, #rest = 1, 2, 3, 4, 5
+first, second, *rest = 1, 2, 3, 4, 5
 p [first, second, rest]   #=> [1, 2, [3, 4, 5]]
 first, *second, rest = 1, 2, 3, 4, 5
 p [first, second, rest]    #=> [1, [2, 3, 4], 5]
