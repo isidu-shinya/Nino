6ff24695578673856342577ec267cda645696625
石津真弥 <ishidushinya@ishidzumayanoMacBook-Pro.local>
2024-04-04 12:09:22 +0900

    chapter8メソッド

--- chapter8#7.rb
+++ chapter8#7.rb
@@ -1,6 +1,6 @@
-def volume(x: , y: 2, z: 4)
-    x * y * z
+def meth(x: 0, y: 0, z: 0, **args)
+    [x, y, z, args]
 end
 
-p volume(x: 2, y: 3)
-p volume(y: 3, z: 4)
+p meth(z: 4, y: 3, x: 2)
+p meth(x: 2, z: 3, v: 4, w: 5)
