8e441c2bc6e6e6e6a34ff22d820f36e8a8dd96d7
石津真弥 <ishidushinya@ishidzumayanoMacBook-Pro.local>
2024-04-07 16:20:21 +0900

    chapter7繰り返しnext練習

--- chapter7#loop3.rb
+++ chapter7#loop3.rb
@@ -4,7 +4,7 @@ max_matches = 10
 matches = 0
 file = File.open(ARGV[0])
 file.each_line do |line|
-   next if /^\s*$/ =~ line
+    next if /^\s*$/ =~ line
     if pattern =~ line
         matches += 1
         puts line
