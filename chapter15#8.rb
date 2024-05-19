fc677ef8e735e4d6439b0985c8703727f2110454
石津真弥 <ishidushinya@ishidzumayanoMacBook-Pro.local>
2024-05-19 13:28:42 +0900

    chapter15第１４章文字列(String)クラスの文字列をつなげる学習

---
+++ chapter15#8.rb
@@ -0,0 +1,14 @@
+#hello = "Hello,"
+#world = "World!"
+
+#str = hello + world
+#p str
+
+hello = "Hello,"
+world = "World!"
+
+hello << world
+p hello
+hello.concat(world)
+p hello
+hello = hello + world
