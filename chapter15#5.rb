461ff1fa8a3189db26034892310ba17d2684762a
石津真弥 <ishidushinya@ishidzumayanoMacBook-Pro.local>
2024-05-19 13:08:06 +0900

    chapter15第１４章文字列(String)クラスの学習

--- chapter15#5.rb
+++ chapter15#5.rb
@@ -1,3 +1,12 @@
 n = 65535
 printf("%dの16進数表記は%xです\n", n , n)
 p sprintf("%dの16進数表記は%xです\n", n, n)
+
+p format("Hello, %s!", "うんこ")
+p "%d年%d月%d日" % [2024, 5, 17]
+
+p sprintf("%8s", "Ruby")
+p sprintf("%8.8s", "Hello Ruby")
+p sprintf("%#010x", 100)
+p sprintf("%+.2f", Math::PI)
+p sprintf("%*.*f", 5, 2, Math::PI)
